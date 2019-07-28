81c56a3f99b11df22194e05c09797e2e4ba72882
Mantas Rajackas <35578357+Matt-RJ@users.noreply.github.com>
2019-07-28 16:34:34 +0100

    Added automatic scroll enabling.
    The script now runs automatically unless disabled after a short delay.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Scroll Bar Enabler
 // @namespace    https://greasyfork.org/users/313414
-// @version      1.0
+// @version      1.1
 // @description  Re-enables the scroll bar for sites that use anti-adblock scripts.
 // @author       Matt-RJ
 // @match        *://*/*
@@ -12,9 +12,31 @@
 (function() {
     'use strict';
 
+    /*
+    Whether the script runs automatically on every page.
+    If set to false, use the extension button -> enable scrolling to run manually.
+    */
+    var automaticallyEnable = true;
+
+    /*
+    Some sites disable scrolling after the page has loaded. enableDelay is how long, in ms,
+    before the script runs automatically to enable scrolling again.
+    */
+    var automaticEnableDelay = 500;
+
+    if (automaticallyEnable) {
+        setTimeout(function() {
+            enableScrolling();
+        }, automaticEnableDelay);
+    }
+
     GM_registerMenuCommand("Enable Scrolling", function() {
+        enableScrolling();
+    }, 'r');
+
+    function enableScrolling() {
         var body = document.getElementsByTagName("BODY")[0];
         body.style.overflow = "visible";
-    }, 'r');
+    }
 
 })();
